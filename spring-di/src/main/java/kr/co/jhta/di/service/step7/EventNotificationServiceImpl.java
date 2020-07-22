91f3de10cb53e1512cc500f4d27fd0c34be08a87
ShimYS <sim5522@naver.com>
2020-07-22 13:50:55 +0900

    2020-07-21 // 2020-07-22 스프링 의존성 주입 학습/연습

---
+++ spring-di/src/main/java/kr/co/jhta/di/service/step7/EventNotificationServiceImpl.java
@@ -0,0 +1,36 @@
+package kr.co.jhta.di.service.step7;
+
+import java.util.List;
+
+import kr.co.jhta.di.service.MessageSender;
+import kr.co.jhta.di.vo.User;
+
+public class EventNotificationServiceImpl implements EventNotificationService{
+	
+	// 인터페이스 - XXX류(범용적)
+	private MessageSender messageSender;
+	private UserService userService;
+	
+	// 주입받는것 제외하고 정의하지 말것
+	// private User user;
+	// DB에서 가져오는건 코딩해서 가져오면됨. 즉, 값을 담는 객체는 조립의 대상이 아님
+	
+	// 객체 조립을 위한 짹
+	public void setMessageSender(MessageSender messageSender) {
+		this.messageSender = messageSender;
+	}
+	
+	public void setUserService(UserService userService) {
+		this.userService = userService;
+	}
+	
+	@Override
+	public void noticeEvent(String eventName, String eventContent) {
+		List<User> users = userService.getAllUser();
+		for(User user : users) {
+			messageSender.send("coders9", user.getTel(), eventName, eventContent);
+			System.out.println("-------------------------------");
+		}
+		
+	}
+}
