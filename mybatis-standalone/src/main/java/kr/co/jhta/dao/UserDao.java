4e5d6529c352fa0afc039ba4aa005fd26ee06d2c
ShimYS <sim5522@naver.com>
2020-07-27 18:06:50 +0900

    2020-07-27 순수 mybatis  사용법 학습

--- mybatis-standalone/src/main/java/kr/co/jhta/dao/UserDao.java
+++ mybatis-standalone/src/main/java/kr/co/jhta/dao/UserDao.java
@@ -2,33 +2,68 @@ package kr.co.jhta.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+
+import kr.co.jhta.util.MybatisUtil;
 import kr.co.jhta.vo.User;
 
 public class UserDao {
 
 	// INSERT: N종류의 데이터, parameter: User
 	public void insertUser(User user) {
-		
+		SqlSession session = MybatisUtil.getSqlSession();
+		try {
+			session.insert("insertUser", user);
+			session.commit();
+		} finally {
+			session.close();
+		}
 	}
 	
 	// DELETE: 없음, parameter: 없음
 	public void deleteAllUsers() {
-		
+		SqlSession session = MybatisUtil.getSqlSession();
+		try {
+			session.delete("deleteAllUsers");
+			session.commit();
+		} finally {
+			session.close();
+		}
 	}
 
 	// DELETE: 기본자료형(String 포함), parameter: string
 	public void deleteUserById(String userId) {
-		
+		SqlSession session = MybatisUtil.getSqlSession();
+		try {
+			session.delete("deleteUserById", userId);
+			session.commit();
+		} finally {
+			session.close();
+		}		
 	}
 	
 	// UPDATE: N종류의 데이터, parameter: User
 	public void updateUser(User user) {
+		SqlSession session = MybatisUtil.getSqlSession();
+		try {
+			session.update("updateUser", user);
+			session.commit();
+		} finally {
+			session.close();
+		}		
 		
 	}
 	
 	// SELECT: N행 N열, parameter: 없음, result: User, 최종값: List<User> 
 	public List<User> getAllUsers() {
-		return null;
+		SqlSession session = MybatisUtil.getSqlSession();
+		try {
+			
+			List<User> users = session.selectList("getAllUsers");
+			return users;
+		} finally {
+			session.close();
+		}
 	}
 	
 	// SELECT: N행 N열, parameter: string, result: User, 최종값: List<User> 
@@ -38,16 +73,34 @@ public class UserDao {
 	
 	// SELECT: 1행 N열, parameter: string, result: User, 최종값: User객체 
 	public User getUserById(String userId) {
-		return null;
+		SqlSession session = MybatisUtil.getSqlSession();
+		try {
+			User user = session.selectOne("getUserById", userId);
+			return user;
+		} finally {
+			session.close();
+		}
 	}
 	
 	// SELECT: 1행 N열, parameter: string, result: User, 최종값: User객체 
 	public User getUserByEmail(String userEmail) {
-		return null;
+		SqlSession session = MybatisUtil.getSqlSession();
+		try {
+			User user = session.selectOne("getUserByEmail", userEmail);
+			return user;
+		} finally {
+			session.close();
+		}
 	}
 	
 	// SELECT: 1행 1열, parameter: 없음, result: int, 최종값: int
 	public int getUsersCount() {
-		return 0;
+		SqlSession session = MybatisUtil.getSqlSession();
+		try {
+			int usersCount = session.selectOne("getUsersCount");
+			return usersCount;
+		} finally {
+			session.close();
+		}
 	}
 }
--- mybatis-standalone/src/main/java/kr/co/jhta/util/MybatisUtil.java
+++ mybatis-standalone/src/main/java/kr/co/jhta/util/MybatisUtil.java
@@ -3,6 +3,7 @@ package kr.co.jhta.util;
 import java.io.InputStream;
 
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
@@ -27,8 +28,7 @@ public class MybatisUtil {
 	 * 			void 	rollback()
 	 * 			쿼리를 한 번 실행할 때 마다 SqlSessionFactory로 부터 새로운
 	 * 			SqlSession객체를 획득해서 사용하고 폐기한다.
-	 * 			
-	 * 		
+	 * 			 		
 	 * 		- SqlSessionFactory => 'Connection Pool이라고 생각하면 된다'
 	 * 			쿼리 실행에 필요한 SqlSession객체를 제공하는 객체다
 	 * 			애플리케이션이 실행되는 동안 딱 한번만 생성해서 계속 사용한다.
@@ -36,26 +36,67 @@ public class MybatisUtil {
 	 * 				* 새로운 SqlSession객체를 반환한다.
 	 * 		
 	 * 		- SqlSessionFactoryBuilder
-	 * 			mybatis 환경설정 정보를 이용해서 SqlSessionfactory객체를 생성한다.
+	 * 			mybatis 환경설정파일(/src/main/resource/mybatis/mybatis-config.xml)을 이용해서 SqlSessionfactory객체를 생성한다.
 	 * 			SqlSessionFactory build(InputStream inputStream)
 	 * 
-	 * 		
 	 */
-	
 	private static SqlSessionFactory sqlSessionFactory;
 	
 	static {
 		try {
-			
+			// mybatis 환경설정파일의 경로 및 파일명 지정
 			String resource = "mybatis/mybatis-config.xml";
+			// mybatis 환경설정파일을 읽어오는 스트림 생성
 			InputStream inputStream = Resources.getResourceAsStream(resource);
+			// 스트림을 이용해서 mybatis 환경설정 파일을 읽어서 SqlSessionFactory객체 획득
 			sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
+	/**
+	 * SqlSessionFactory 객체를 반환한다.
+	 * @return SqlSessionFactory객체
+	 */
 	public static SqlSessionFactory getSqlSessionFactory() {
 		return sqlSessionFactory;
 	}
+	
+	/**
+	 * SQL 구문을 실행하는 SqlSession 객체를 반환한다.
+	 * @return SqlSession 객체 (INSERT/UPDATE/DELETE/SELECT, 트랜잭션 기능 제공)
+	 */
+	public static SqlSession getSqlSession() {
+		return sqlSessionFactory.openSession();
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
